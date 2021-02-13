8a03e236b8cd99db29922e3195b69f3923b5eacd
Pani Praveen <69384743+panipraveen@users.noreply.github.com>
2021-02-13 14:42:51 +0530

    Add files via upload

---
+++ event_details.php
@@ -0,0 +1,108 @@
+<?php require_once("assets/include/include.php") ?>
+<html>
+	<head>
+		<title>Home Page</title>
+		<link rel="stylesheet" href="assets/css/style.css">
+		<link rel="stylesheet" href="assets/css/event_details_style.css">
+	</head>
+	<body>
+		<div class="container">
+			<ul id="navbar">
+			  <li><a href="#home">Home</a></li>
+			  <li><a href="#news">Events</a></li>
+			  <li><a href="#contact">Contact</a></li>
+			  <li><a href="#about" class="reg_btn">Register Now</a></li>
+			  <li class="list_right"><img src="assets/images/Ziegers title.png" class="img_work"></li>
+			</ul>
+		</div>
+		<div class="container">
+			<div id="pot">
+			  <img src="assets/images/Ellipse 5.png" width=100px height=100px>
+			</div>
+			<div id="pot1">
+			  <img src="assets/images/Ellipse 6.png" width=100px height=100px>
+			</div>
+			<div id="pot2">
+			  <img src="assets/images/Ellipse 7.png" width=100px height=100px>
+			</div>
+			<div id="pot3">
+			  <img src="assets/images/Ellipse 8.png" width=100px height=100px>
+			</div>
+		</div>
+		<div class="container">
+			<img src="assets/images/codex1.png" class="top_image_Work"><br>
+			<img src="assets/images/codex2.png" class="ziegers_right">
+			<div class="left_line"><img src="assets/images/Rectangle 27.png"></div>
+			<div class="main_image"><img src="assets/images/demo_codex.jpg" class="demo_main"></div>
+			<div class="codex_title"><h3>CODEX <img src="assets/images/Rectangle 28.png" class="top_hori"></h3></div>
+			<p class="codex_content">
+				Computer science is the study of algorithmic processes, computational
+				machines and computation itself. As a discipline, computer science spans a range of topics from theoretical studies of algorithms,
+				computation and information to the practical issues of implementing computational systems in hardware and software.
+				Computer science is the study of algorithmic processes,
+				computational machines and computation itself. As a discipline, computer science spans a range of topics from theoretical studies of algorithms,
+				computation and information to the practical issues of implementing computational systems in hardware and software.
+				Computer science is the study of algorithmic processes, computational
+				machines and computation itself. As a discipline, computer science spans a range of topics from theoretical studies of algorithms,
+				computation and information to the practical issues of implementing computational systems in hardware and software.
+				Computer science is the study of algorithmic processes,
+				computational machines and computation itself. As a discipline, computer science spans a range of topics from theoretical studies of algorithms,
+				computation and information to the practical issues of implementing computational systems in hardware and software.
+				Computer science is the study of algorithmic processes, computational
+				machines and computation itself. As a discipline, computer science spans a range of topics from theoretical studies of algorithms,
+				<br><br>
+				<button type="button" class="register_btn">REGISTER NOW</button>
+			</p>
+			<div class="bottom_line"><img src="assets/images/Rectangle 29.png"></div><br><br>
+			<div class="other_title">OTHER EVENTS</div>
+			<div class="row">
+				<div class="col-md-3 col-xs-3">
+					<div class="box one">
+						<div class="top_first"><img src="assets/images/codex.jpg" height=200px,width=300px></div>
+						<div class="bottom_last">noun. Usually contents.
+						something tht is contained: the contents of a box. the subjects or topics covered in a book or document.
+						the chapters or other formal divisions of a book or document: a table of contents.</div>
+					</div>
+				</div>
+				<div class="col-md-1 col-xs-2"></div>
+				<div class="col-md-3 col-xs-3">
+					<div class="box one">
+						<div class="top_first"><img src="assets/images/codex.jpg" height=200px,width=300px></div>
+						<div class="bottom_last">noun. Usually contents.
+						something tht is contained: the contents of a box. the subjects or topics covered in a book or document.
+						the chapters or other formal divisions of a book or document: a table of contents.</div>
+					</div>
+				</div>
+				<div class="col-md-1 col-xs-2"></div>
+				<div class="col-md-3 col-xs-2">
+					<div class="box one hmm">
+						<div class="top_first"><img src="assets/images/codex.jpg" height=200px,width=300px></div>
+						<div class="bottom_last">noun. Usually contents.
+						something tht is contained: the contents of a box. the subjects or topics covered in a book or document.
+						the chapters or other formal divisions of a book or document: a table of contents.</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div id="pot4">
+			  <img src="assets/images/Ellipse 5.png" width=100px height=100px>
+			</div>
+			<div id="pot5">
+			  <img src="assets/images/Ellipse 6.png" width=100px height=100px>
+			</div>
+			<div id="pot6">
+			  <img src="assets/images/Ellipse 7.png" width=100px height=100px>
+			</div>
+			<div id="pot7">
+			  <img src="assets/images/Ellipse 8.png" width=100px height=100px>
+			</div>
+			<div id="pot8">
+			  <img src="assets/images/Ellipse 8.png" width=100px height=100px>
+			</div>
+			<div id="pot9">
+			  <img src="assets/images/Ellipse 8.png" width=100px height=100px>
+			</div>
+		</div>
+	</body>
+</html>
---
+++ index.php
@@ -0,0 +1,165 @@
+<?php require_once("assets/include/include.php") ?>
+<html>
+	<head>
+		<title>Home Page</title>
+		<link rel="stylesheet" href="assets/css/style.css">
+	</head>
+	<body>
+		<div class="container">
+			<ul id="navbar">
+			  <li><a href="#home">Home</a></li>
+			  <li><a href="#news">Events</a></li>
+			  <li><a href="#contact">Contact</a></li>
+			  <li><a href="#about" class="reg_btn">Register Now</a></li>
+			  <li class="list_right"><img src="assets/images/Ziegers title.png" class="img_work"></li>
+			</ul>
+		</div>
+		<div class="container">
+			<div id="pot">
+			  <img src="assets/images/Ellipse 5.png" width=100px height=100px>
+			</div>
+			<div id="pot1">
+			  <img src="assets/images/Ellipse 6.png" width=100px height=100px>
+			</div>
+			<div id="pot2">
+			  <img src="assets/images/Ellipse 7.png" width=100px height=100px>
+			</div>
+			<div id="pot3">
+			  <img src="assets/images/Ellipse 8.png" width=100px height=100px>
+			</div>
+		</div>
+		<div class="container">
+			<img src="assets/images/ZIEGERS.png" width="70%" height=100px><br>
+			<img src="assets/images/ZIEGERS-1.png" class="ziegers_1">
+			<br>
+			<h3 class="ziegers_2021_hd"> ZIEGERS 2021 <div class="new_line"></div></h3>
+			<p class="zieger_content">
+				noun. Usually contents.
+				something tht is contained: the contents of a box. the subjects or topics covered in a book or document.
+				the chapters or other formal divisions of a book or document: a table of contents.
+				noun. Usually contents.a
+				something that is contained: the contents of a box. the subjects or topics covered in a book or document.
+				the chapters or other formal divisions of a book or document: a table of contents.
+				
+				<br><br>
+					<button type="button" class="register_btn">REGISTER NOW</button>
+				<br><br>
+			</p><br><br>
+			<h3 class="ziegers_2021_hd"> OUR EVENT <div class="new_line"></div></h3><br>
+			
+			<div class="whole_row">
+				<div class="box one">
+					<div class="top_first"><img src="assets/images/codex.jpg" height=200px,width=300px></div>
+					<div class="bottom_last">noun. Usually contents.
+					something tht is contained: the contents of a box. the subjects or topics covered in a book or document.
+					the chapters or other formal divisions of a book or document: a table of contents.</div>
+				</div>
+				<div class="box one">
+					<div class="top_first"><img src="assets/images/codex.jpg" height=200px,width=300px></div>
+					<div class="bottom_last">noun. Usually contents.
+					something tht is contained: the contents of a box. the subjects or topics covered in a book or document.
+					the chapters or other formal divisions of a book or document: a table of contents.</div>
+				</div>
+				<div class="box one">
+					<div class="top_first"><img src="assets/images/codex.jpg" height=200px,width=300px></div>
+					<div class="bottom_last">noun. Usually contents.
+					something tht is contained: the contents of a box. the subjects or topics covered in a book or document.
+					the chapters or other formal divisions of a book or document: a table of contents.</div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div id="pot4">
+			  <img src="assets/images/Ellipse 5.png" width=100px height=100px>
+			</div>
+			<div id="pot5">
+			  <img src="assets/images/Ellipse 6.png" width=100px height=100px>
+			</div>
+			<div id="pot6">
+			  <img src="assets/images/Ellipse 7.png" width=100px height=100px>
+			</div>
+			<div id="pot7">
+			  <img src="assets/images/Ellipse 8.png" width=100px height=100px>
+			</div>
+		</div><br><br>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-7 col-sm-7 col-xs-7">
+					<h3 class="ziegers_2021_hd"> ABOUT US <div class="new_line"></div></h3><br>
+					<p class="zieger_content">The about us page is commonly used by all types of businesses to give customers more insight 
+					into who is involved with a given business and exactly what it does. ...
+					The about us page is often a reflection of the purpose and personality of the business and its owners or top employees.
+					The about us page is commonly used by all types of businesses to give customers more insight 
+					into who is involved with a given business and exactly what it does. ...
+					The about us page is often a reflection of the purpose and personality of the business and its owners or top employees.
+					</p>
+				</div><br><br><br><br>
+				<div class="col-md-5 col-sm-5 col-xs-5">
+					<img src="assets/images/codex.jpg" height=430px,width=430px>
+				</div>
+			</div>
+		</div><br><br>
+		<div class="container">
+			<h3 class="head_of_event">HEAD OF THE EVENTS<div class="new_line_1"></div></h3><br><br>
+			<div class="row">
+				<div class="col-md-3 col-sm-3"><div class="img_1"></div></div>
+				<div class="col-md-1 col-sm-2"></div>
+				<div class="col-md-3 col-sm-3"><div class="img_1"></div></div>
+				<div class="col-md-1 col-sm-2"></div>
+				<div class="col-md-3 col-sm-2"><div class="img_1"></div></div>
+			</div><br>
+			<div class="row">
+				<div class="col-md-3 col-sm-3"><div class="desc_1"></div></div>
+				<div class="col-md-1 col-sm-2"></div>
+				<div class="col-md-3 col-sm-3"><div class="desc_1"></div></div>
+				<div class="col-md-1 col-sm-2"></div>
+				<div class="col-md-3 col-sm-2"><div class="desc_1"></div></div>
+			</div>
+		</div><br><br>
+		<div class="container">
+			<h3 class="ziegers_2021_hd"> OUR SPONSORS <div class="new_line"></div></h3><br>
+			<div class="whole">
+				<div class="row">
+					<div class="col-md-2 col-sm-2"><img src="assets/images/md.png" class="logo_work"></div>
+					<div class="col-md-1 col-sm-2"></div>
+					<div class="col-md-2 col-sm-2"><img src="assets/images/md.png" class="logo_work"></div>
+					<div class="col-md-1 col-sm-2"></div>
+					<div class="col-md-2 col-sm-2"><img src="assets/images/md.png" class="logo_work"></div>
+					<div class="col-md-1 col-sm-2"></div>
+					<div class="col-md-2 col-sm-2"><img src="assets/images/md.png" class="logo_work"></div>
+				</div><br><br><br>
+				<div class="row">
+					<div class="col-md-1 col-sm-1"></div>
+					<div class="col-md-2 col-sm-2"><img src="assets/images/md.png" class="logo_work"></div>
+					<div class="col-md-1 col-sm-2"></div>
+					<div class="col-md-2 col-sm-2"><img src="assets/images/md.png" class="logo_work"></div>
+					<div class="col-md-1 col-sm-2"></div>
+					<div class="col-md-2 col-sm-2"><img src="assets/images/md.png" class="logo_work"></div>
+				</div>
+			</div>
+		</div>
+		<div class="container">
+			<div id="pot8">
+			  <img src="assets/images/Ellipse 5.png" width=100px height=100px>
+			</div>
+			<div id="pot9">
+			  <img src="assets/images/Ellipse 6.png" width=100px height=100px>
+			</div>
+			<div id="pot10">
+			  <img src="assets/images/Ellipse 7.png" width=100px height=100px>
+			</div>
+			<div id="pot11">
+			  <img src="assets/images/Ellipse 8.png" width=100px height=100px>
+			</div>
+			<div id="pot12">
+			  <img src="assets/images/Ellipse 8.png" width=100px height=100px>
+			</div>
+			<div id="pot13">
+			  <img src="assets/images/Ellipse 8.png" width=100px height=100px>
+			</div>
+			<div id="pot14">
+			  <img src="assets/images/Ellipse 8.png" width=100px height=100px>
+			</div>
+		</div><br><br>
+	</body>
+</html>
